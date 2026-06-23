26a6d35f4f1161ca27c7bd8e685b05c6f7011282
snatera <snatera.ve@gmail.com>
2026-06-23 19:10:36 -0300

    Fix serverless

---
+++ signedurl/handler.js
@@ -0,0 +1,21 @@
+const AWS = require("aws-sdk")
+
+const s3 = new AWS.S3({ signatureVersion: 'v4' })
+
+const signedS3URL = async (event, context) => {
+    const filename = event.queryStringParameters.filename
+    const signedUrl = await s3.getSignedUrlPromise("putObject", {
+        Key: `upload/${filename}`,
+        Bucket: process.env.BUCKET,
+        Expires: 300,
+    });
+    console.log(' en el cloudwatch')
+    return {
+        "statusCode": 200,
+        "body": JSON.stringify({ signedUrl })
+    }
+}
+
+module.exports = {
+    signedS3URL
+}
